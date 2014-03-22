5ae4d22a408d003239daee63706e5b221aad7c43
VijoVShepHertz <vijo.varghese@shephertz.co.in>
2014-03-22 17:54:52 +0530

    html css updated

--- App42PaaS_Python_MongoDB_Sample/urls.py
+++ App42PaaS_Python_MongoDB_Sample/urls.py
@@ -5,13 +5,9 @@ admin.autodiscover()
 
 urlpatterns = patterns('',
     # Examples:
-    # url(r'^$', 'App42PaaS_Python_MongoDB_Sample.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
-	
-	# sample
-	#   (r'^sample/', include('sample.urls')),
-	    url(r'^$', 'sample.views.index'),
-	    url(r'^sample/save', 'sample.views.save'),
+	url(r'^$', 'sample.views.index'),
+	url(r'^sample/save', 'sample.views.save'),
 )
